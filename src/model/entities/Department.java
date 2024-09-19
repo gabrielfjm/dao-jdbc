e4fba006697b608bebd3e893329635e8aa302408
Gabriel Felipe Jess Meira <bibodev04@gmail.com>
2024-09-19 15:22:18 -0300

    Seller entity class

--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -3,14 +3,14 @@ package model.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
-public class Department implements Serializable{
+public class Department implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	
 	private Integer id;
 	private String name;
 	
-	public Department () {
+	public Department() {
 		
 	}
 
